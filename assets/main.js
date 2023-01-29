84677b400581cd26f76c0d8594038709f3108656
RaiNonat <68254913+RaimundoNonat@users.noreply.github.com>
2023-01-29 01:11:11 -0300

    Add files via upload

---
+++ assets/main.js
@@ -0,0 +1,11 @@
+function linkedin() {
+    window.location.href = "https://br.linkedin.com/"
+}
+
+function github() {
+    window.location.href = "https://github.com/"
+}
+
+function projetos() {
+    window.location.href = "./pages/projetos/index.html"
+}
